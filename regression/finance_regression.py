80202f8bac488aaaca60d60e0e5026dfe43e92df
franzaltea <franzaltea@gmail.com>
2018-07-22 15:03:48 -0400

    Finished Linear Regression for Finance data

--- regression/finance_regression.py
+++ regression/finance_regression.py
@@ -18,6 +18,8 @@ import pickle
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
 dictionary = pickle.load( open("../final_project/final_project_dataset_modified.pkl", "r") )
+from sklearn import linear_model
+from sklearn.metrics import r2_score
 
 ### list the features you want to look at--first item in the 
 ### list will be the "target" feature
@@ -29,7 +31,7 @@ target, features = targetFeatureSplit( data )
 from sklearn.cross_validation import train_test_split
 feature_train, feature_test, target_train, target_test = train_test_split(features, target, test_size=0.5, random_state=42)
 train_color = "b"
-test_color = "b"
+test_color = "r"
 
 
 
@@ -37,11 +39,12 @@ test_color = "b"
 ### Please name it reg, so that the plotting code below picks it up and 
 ### plots it correctly. Don't forget to change the test_color above from "b" to
 ### "r" to differentiate training points from test points.
+reg = linear_model.LinearRegression()
+reg.fit(feature_train, target_train)
+pred = reg.predict(feature_test)
 
-
-
-
-
+print "The coefficients are m: {0} and b: {1}".format(reg.coef_, reg.intercept_)
+print "R^2: {0}".format(r2_score(target_test, pred))
 
 
 
